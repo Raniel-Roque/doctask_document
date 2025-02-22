1088b46b65e0d9d48df64728fda8958c0c2285a6
raniel-roque <raniel.roque0509@gmail.com>
2025-02-22 19:01:04 +0800

    Finished Font Family / Heading following Capstone Manual

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
 import Underline from '@tiptap/extension-underline'
 import { useEditorStore } from '@/store/use-editor-store';
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
 
 export const Editor = () => {
     const { setEditor } = useEditorStore();
@@ -43,10 +45,11 @@ export const Editor = () => {
         },
         editorProps: {
             attributes: {
-                style: "padding-left: 56px; padding-right: 56px;",
+                style: "padding-left: 56px; padding-right: 56px; font-family: 'Times New Roman', serif;",
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
         },
+        
         extensions: [
             StarterKit,
             TaskList,
@@ -62,22 +65,11 @@ export const Editor = () => {
             TableCell,
             ImageResize,
             Underline,
+            FontFamily,
+            TextStyle
         ],
         content: `
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
+
       `,
     })
 
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,10 +1,111 @@
 "use client";
 
-import { BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
 import { Separator } from "@/components/ui/separator";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import { type Level } from "@tiptap/extension-heading";
 
+const FontFamilyButton = () => {
+    const { editor } = useEditorStore();
+
+    const fonts = [
+        { label: "Times New Roman", value:"Times New Roman" },
+        { label: "Arial", value:"Arial" },
+        { label: "Courier New", value:"Courier New" },
+        { label: "Georgia", value:"Georgia" },
+        { label: "Verdana", value:"Verdana" }
+    ];
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button className="h-7 w-[120px] shrink-0 flex item-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                    <span className="truncate">
+                        {editor?.getAttributes("textStyle").fontFamily || "Times New Roman"}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p=1 flex flex-col gap-y-1">
+                {fonts.map(({ label, value }) => (
+                    <button 
+                        onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+                        key={value} 
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                            editor?.getAttributes("textStyle").fontFamily === value && "hover:bg-neutral-200/80"
+                        )}
+                        style={{ fontFamily: value }}
+                    >
+                        <span className="text-sm">{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+const HeadingLevelButton = () => {
+    const { editor } = useEditorStore();
+
+    const headings = [
+        { label: "Normal Text", value: 0, fontSize: "16px" },
+        { label: "Heading 1", value: 1, fontSize: "32px" },
+        { label: "Heading 2", value: 2, fontSize: "24px" },
+        { label: "Heading 3", value: 3, fontSize: "20px" },
+        { label: "Heading 4", value: 4, fontSize: "18px" },
+        { label: "Heading 5", value: 5, fontSize: "16px" },
+    ];
+
+    const getCurrentHeading = () => {
+        for ( let level = 1; level <= 5; level++) {
+            if (editor?.isActive("heading", { level })) {
+                return `Heading ${level}`;
+            }
+        }
+
+        return "Normal text";
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button className="h-7 min-w-7 shrink-0 flex item-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                    <span className="truncate">
+                        {getCurrentHeading()}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {headings.map(({ label, value, fontSize}) => (
+                    <button 
+                        key={value}
+                        style={{ fontSize }}
+                        onClick={() => {
+                            if (value === 0) {
+                                editor?.chain().focus().setParagraph().unsetBold().run(); // Remove bold for normal text
+                            } else if (value === 1 || value === 2) {
+                                editor?.chain().focus().toggleHeading({ level: value as Level }).setBold().run(); // Apply bold for h1 and h2
+                            } else {
+                                editor?.chain().focus().toggleHeading({ level: value as Level }).unsetBold().run(); // Ensure no bold for h3, h4, h5
+                            }
+                        }}
+                        
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                            (value === 0 && !editor?.isActive("heading")) ||  editor?.isActive("heading", {level: value}) && "hover:bg-neutral-200/80"
+                        )}
+                    >
+                        {label}
+                    </button>                
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 interface ToolbarButtonProps {
     onClick?: () => void;
     isActive?: boolean;
@@ -67,7 +168,7 @@ export const Toolbar = () => {
             {
                 label: "Bold",
                 icon: BoldIcon,
-                isActive: editor?.isActive("bold"),
+                isActive: editor?.isActive("bold") || editor?.isActive("heading", { level: 1 }) || editor?.isActive("heading", { level: 2 }),
                 onClick: () => editor?.chain().focus().toggleBold().run(),
             },
             {
@@ -109,9 +210,9 @@ export const Toolbar = () => {
                 <ToolbarButton key={item.label} {...item}/>
             ))}
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-            {/* TODO Font Fam */}
+            <FontFamilyButton />
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-            {/* TODO Heading */}
+            <HeadingLevelButton />
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
             {/* TODO Font Size */}
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
