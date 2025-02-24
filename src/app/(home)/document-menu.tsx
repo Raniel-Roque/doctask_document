10892ff9fb1305b70d55c5428fcc03c87824288a
raniel-roque <raniel.roque0509@gmail.com>
2025-02-24 12:38:24 +0800

    Added Delete and Rename/Edit Docs

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,52 @@
+import { ExternalLinkIcon, FilePenIcon, MoreVertical, TrashIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Id } from "../../../convex/_generated/dataModel";
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { RemoveDialog } from "./remove-dialog";
+import { RenameDialog } from "./rename-dialog";
+
+interface DocumentMenuProps {
+    documentId: Id<"documents">
+    title: string;
+    onNewTab: (id: Id<"documents">) => void;
+}
+export const DocumentMenu = ({ documentId, title, onNewTab }: DocumentMenuProps) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="rounded-full">
+                    <MoreVertical className="size-4"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <RenameDialog documentId={documentId}>
+                    <DropdownMenuItem
+                        onSelect={(e) => e.preventDefault()}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <FilePenIcon className="mr-2 size-4" />
+                        Rename
+                    </DropdownMenuItem>
+                </RenameDialog>
+                <RemoveDialog documentId={documentId}>
+                    <DropdownMenuItem
+                        onSelect={(e) => e.preventDefault()}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <TrashIcon className="mr-2 size-4" />
+                        Delete
+                    </DropdownMenuItem>
+                </RemoveDialog>
+                <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+                    <ExternalLinkIcon className="size-4 mr-2"/>
+                    Open in a new tab
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )   
+}
---
+++ src/app/(home)/rename-dialog.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Id } from "../../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface RenameDialogProps {
+    documentId: Id<"documents">;
+    children: React.ReactNode;
+}
+
+export const RenameDialog = ({ documentId, children }: RenameDialogProps ) => {
+    const remove = useMutation(api.documents.removeById);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                {children}                
+            </DialogTrigger>
+            <DialogContent onClick={(e) => e.stopPropagation()}>
+                <form>
+                    <DialogHeader>
+                        <DialogTitle>Rename document</DialogTitle>
+                        <DialogDescription>
+                            Enter a new name for this document
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="my-4">
+                        <Input />
+                    </div>
+                    <DialogFooter>
+                        <Button>
+                            Cancel
+                        </Button>
+                        <Button>
+                            Save
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
